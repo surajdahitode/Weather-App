9e0bc8df72118c6c058cc1aa31203eed857629eb
surajdahitode <154809605+surajdahitode@users.noreply.github.com>
2024-02-15 13:05:43 +0530

    upadate the code

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 async function fetchWheatherCity(cityName) {
-    let url=https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a6821afe4630c8b189f6a64e5950bcb;
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a6821afe4630c8b189f6a64e5950bcb`
     const response=await fetch(url);
     const data=await response.json();
     let city=document.getElementById("city");
@@ -8,7 +8,7 @@ async function fetchWheatherCity(cityName) {
     windSpeed.innerHTML=data.wind.speed +"km/hr";
     let humidity= document.getElementById("humidity");
     humidity.innerHTML=data.main.humidity+"g/kg";
-    let temp=document.getElementById("temp");
+    let temp=document.getElementById("temp"); 
     temp.innerHTML=Math.round((data.main.temp)-273.15)+"°C"
     console.log(data)
 }
